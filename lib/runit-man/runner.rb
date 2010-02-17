c1c8134e3ba43343179e824cb91d353f564bf84b
Akzhan Abdulin <akzhan.abdulin@gmail.com>
2010-02-17 23:37:39 +0300

    No functionality in scripts because they do not updated usually by rubygems.

---
+++ lib/runit-man/runner.rb
@@ -0,0 +1,20 @@
+require 'optparse'
+require 'runit-man/app'
+
+RunitMan.set :active_services_directory, '/etc/service'
+RunitMan.set :all_services_directory,    '/etc/sv'
+
+OptionParser.new { |op|
+  op.on('-s server') { |val| RunitMan.set :server, val }
+  op.on('-p port')   { |val| RunitMan.set :port, val.to_i }
+  op.on('-b addr')   { |val| RunitMan.set :bind, val } if RunitMan.respond_to?(:bind)
+  op.on('-a active_services_directory (/etc/service by default)') { |val| RunitMan.set :active_services_directory, val }
+  op.on('-f all_services_directory (/etc/sv by default)')         { |val| RunitMan.set :all_services_directory, val }
+  op.on_tail('-r', '--register', 'Register as runit service') do
+    RunitMan.register_as_runit_service
+    exit
+  end
+}.parse!(ARGV.dup)
+
+RunitMan.run!
+
