af0e527ade6040d0898d89806fc63518d1391efc
Akzhan Abdulin <akzhan.abdulin@gmail.com>
2010-02-26 16:54:47 +0300

    Fix absence in DNS error.

--- lib/runit-man/helpers.rb
+++ lib/runit-man/helpers.rb
@@ -13,7 +13,11 @@ module Helpers
 
   def host_name
     unless @host_name
-      @host_name = Socket.gethostbyname(Socket.gethostname).first
+      begin
+        @host_name = Socket.gethostbyname(Socket.gethostname).first
+      rescue
+        @host_name = Socket.gethostname
+      end
     end
     @host_name
   end
