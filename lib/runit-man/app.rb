2895c88a643d532cb419f846c2a2979c173716da
Akzhan Abdulin <akzhan.abdulin@gmail.com>
2011-05-25 15:49:08 +0400

    Upgrade jQuery to 1.6.1

--- lib/runit-man/app.rb
+++ lib/runit-man/app.rb
@@ -108,7 +108,7 @@ class RunitMan < Sinatra::Base
   end
 
   get '/' do
-    @scripts = %w[ jquery-1.6.min runit-man ]
+    @scripts = %w[ jquery-1.6.1.min runit-man ]
     @title = host_name
     haml :index
   end
