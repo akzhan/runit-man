9d8cf2669086553b1cb24185d38eb61563a6b621
Akzhan Abdulin <akzhan.abdulin@gmail.com>
2011-05-30 18:41:39 +0400

    Backport Sinatra 1.3.0 functionality of using Rack::File to serve static files.

--- lib/runit-man/app.rb
+++ lib/runit-man/app.rb
@@ -14,6 +14,37 @@ if RUBY_VERSION >= '1.9'
   Encoding.default_internal = "utf-8"
 end
 
+if Sinatra::VERSION < '1.3.0' && Rack.release >= '1.3'
+  # Monkey patch old Sinatra to use Rack::File to serve files.
+  $stdout.puts 'Use custom Rack::File send_file'
+  $stdout.flush
+
+  Sinatra::Helpers.class_eval do
+    # Got from Sinatra 1.3.0 sources
+    def send_file(path, opts={})
+      if opts[:type] or not response['Content-Type']
+        content_type opts[:type] || File.extname(path), :default => 'application/octet-stream'
+      end
+
+      if opts[:disposition] == 'attachment' || opts[:filename]
+        attachment opts[:filename] || path
+      elsif opts[:disposition] == 'inline'
+        response['Content-Disposition'] = 'inline'
+      end
+
+      last_modified opts[:last_modified] if opts[:last_modified]
+
+      file      = Rack::File.new nil
+      file.path = path
+      result    = file.serving env
+      result[1].each { |k,v| headers[k] ||= v }
+      halt result[0], result[2]
+    rescue Errno::ENOENT
+      not_found
+    end
+  end
+end
+
 class RunitMan < Sinatra::Base
   VERSION       = RunitManVersion::VERSION
   MIN_TAIL      = 100
