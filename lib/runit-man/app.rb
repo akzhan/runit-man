edb89694e0c5d7ecfd1fee91d4759c5df535ac07
Akzhan Abdulin <akzhan.abdulin@gmail.com>
2011-11-03 15:37:07 +0300

    Update to Sinatra 1.3

--- lib/runit-man/app.rb
+++ lib/runit-man/app.rb
@@ -14,39 +14,7 @@ if RUBY_VERSION >= '1.9'
   Encoding.default_internal = "utf-8"
 end
 
-RunitManVersion.sendfile = (Sinatra::VERSION >= '1.3.0')
-
-if Sinatra::VERSION < '1.3.0' && Rack.release >= '1.3'
-  # Monkey patch old Sinatra to use Rack::File to serve files.
-  RunitManVersion.sendfile = true
-
-  Sinatra::Helpers.class_eval do
-    # Got from Sinatra 1.3.0 sources
-    def send_file(path, opts={})
-      if opts[:type] or not response['Content-Type']
-        content_type opts[:type] || File.extname(path), :default => 'application/octet-stream'
-      end
-
-      if opts[:disposition] == 'attachment' || opts[:filename]
-        attachment opts[:filename] || path
-      elsif opts[:disposition] == 'inline'
-        response['Content-Disposition'] = 'inline'
-      end
-
-      last_modified opts[:last_modified] if opts[:last_modified]
-
-      file      = Rack::File.new nil
-      file.path = path
-      result    = file.serving env
-      result[1].each { |k,v| headers[k] ||= v }
-      halt result[0], result[2]
-    rescue Errno::ENOENT
-      not_found
-    end
-  end
-end
-
-RunitManVersion.sendfile &&= !!File.instance_methods.detect { |m| "#{m}" == 'trysendfile' }
+RunitManVersion.sendfile = !!File.instance_methods.detect { |m| "#{m}" == 'trysendfile' }
 
 class RunitMan < Sinatra::Base
   VERSION       = RunitManVersion::VERSION
--- runit-man.gemspec
+++ runit-man.gemspec
@@ -18,18 +18,18 @@ spec = Gem::Specification.new do |s|
   ]
   s.files = `git ls-files`.split("\n")
   s.test_files = `git ls-files -- {test,spec,features}/*`.split("\n")
-  s.add_dependency 'yajl-ruby', '>= 0.7.8'
-  s.add_dependency 'haml', '>= 3.0'
-  s.add_dependency 'sinatra', '>= 1.1'
-  s.add_dependency 'sinatra-content-for2', '>= 0.2.4'
-  s.add_dependency 'i18n', '>= 0.5.0'
-  s.add_dependency 'file-tail', '>= 1.0.5'
+  s.add_dependency 'yajl-ruby', '~> 1.0'
+  s.add_dependency 'haml', '~> 3.0'
+  s.add_dependency 'sinatra', '~> 1.3'
+  s.add_dependency 'sinatra-content-for2', '~> 0.2.4'
+  s.add_dependency 'i18n', '~> 0.5'
+  s.add_dependency 'file-tail', '~> 1.0.5'
   s.add_development_dependency 'rake', '!= 0.9.0'
   s.add_development_dependency 'rspec-core'
   s.add_development_dependency 'rspec-expectations'
   s.add_development_dependency 'rr'
   s.add_development_dependency 'rack-test'
-  s.add_development_dependency 'bundler', '>= 1.0.10'
+  s.add_development_dependency 'bundler', '~> 1.0.10'
   s.description = File.open(File.join(File.dirname(__FILE__), 'DESCRIPTION')).read
 end
 
