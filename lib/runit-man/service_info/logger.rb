76fc42b4062abb0a228ce999687e5747509f05ca
Akzhan Abdulin <akzhan.abdulin@gmail.com>
2012-04-03 15:02:11 +0400

    Ability to show all log files including created ones by rsyslog rules.

--- lib/runit-man/service_info/logger.rb
+++ lib/runit-man/service_info/logger.rb
@@ -20,29 +20,29 @@ class ServiceInfo::Logger < ServiceInfo::Base
     return []  unless File.directory?(dir_name)
 
     r = []
-    Dir.foreach(dir_name) do |name|
-      next  if ServiceInfo::Base.itself_or_parent?(name)
-      full_name = File.expand_path(name, dir_name)
-      next  unless File.directory?(full_name)
-      file_name = File.join(full_name, "#{self.name}.log")
-      label = "#{Utils.host_name}-#{self.name}-#{name}.log"
+    Dir.foreach(dir_name) do |subdirname|
+      next  if ServiceInfo::Base.itself_or_parent?(subdirname)
+      subdirpath = File.expand_path(subdirname, dir_name)
+      next  unless File.directory?(subdirpath)
 
-      unless File.exists?(file_name)
-        file_name = "#{file_name}.gz"
-        label = "#{label}.gz"
+      Dir.foreach(subdirpath) do |filename|
+        next  if ServiceInfo::Base.itself_or_parent?(filename)
+        filepath = File.expand_path(filename, subdirpath)
+        label = "#{Utils.host_name}-#{filename}"
+        next  unless File.file?(filepath) && File.readable?(filepath)
+
+        stats = File.stat(file_name)
+        stat_times = [stats.ctime.utc, stats.atime.utc, stats.mtime.utc]
+        min_time, max_time = stat_times.min, stat_times.max
+
+        r << {
+          :name     => name,
+          :label    => label,
+          :size     => stats.size,
+          :created  => min_time,
+          :modified => max_time
+        }
       end
-
-      stats = File.stat(file_name)
-      stat_times = [stats.ctime.utc, stats.atime.utc, stats.mtime.utc]
-      min_time, max_time = stat_times.min, stat_times.max
-
-      r << {
-        :name     => name,
-        :label    => label,
-        :size     => stats.size,
-        :created  => min_time,
-        :modified => max_time
-      }
     end
     sorted_log_files(r)
   end
